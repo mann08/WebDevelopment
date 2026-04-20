21503f92a99104140c16f7a146cda245d17a8d99
mann08 <mann882005@gmail.com>
2026-04-21 00:48:31 +0530

    Add initial HTML structure, JavaScript file, and TypeScript configuration

---
+++ JSBasic/script.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+console.log("MK1108");
+
+let x = 102;
+let y = '102';
+
+console.log(x == y); // true
+console.log(x === y); // false
+console.log(typeof x); // number
+console.log(typeof y); // string
+
+
+var a = 10;
+var b = '10';
+
+console.log(a == b); // true
+console.log(a === b); // false
