cea7a3f6af5415deaae33f8bdf831dda64db8dca
mann08 <mann882005@gmail.com>
2026-04-30 19:28:41 +0530

    Refactor conditional statements and improve code readability; add login validation and logical operations examples.

--- JSConditionalStatement/script.js
+++ JSConditionalStatement/script.js
@@ -4,18 +4,21 @@ if (n > 0) {
 } else {
   console.log("The number is not positive");
 }
+
 let age = 18;
 if (age >= 18) {
   console.log("You are eligible to vote.");
 } else {
   console.log("You are not eligible to vote.");
 }
+
 let num = 10;
 if (num % 2 === 0) {
   console.log("The number is even.");
 } else {
   console.log("The number is odd.");
 }
+
 let score = 85;
 if (score >= 90) {
   console.log("Grade: A");
@@ -28,3 +31,27 @@ if (score >= 90) {
 } else {
   console.log("Grade: Fail");
 }
+
+let username = "admin";
+let password = "password123";
+if (username === "admin" && password === "password123") {
+  console.log("Login successful!");
+} else {
+  console.log("Invalid username or password.");
+}
+
+let a = 5,
+  b = 6;
+console.log(a > 3 && b < 10);
+console.log(a > 3 || b < 5);
+
+let data;
+let name = "Mann";
+
+data = name|| "N/A";
+// if (name) {
+//   data = name;
+// } else {
+//   data = "N/A";
+// }
+console.log(data);
