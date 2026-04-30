0cbceb5c8f92cbf65afb5c9aafb0b9d973c137e8
mann08 <mann882005@gmail.com>
2026-04-30 18:49:01 +0530

    Add HTML and JavaScript files for Conditional Statement examples

---
+++ JSConditionalStatement/script.js
@@ -0,0 +1,20 @@
+let n = 5;
+if (n > 0) {
+  console.log("The number is positive");
+} else {
+  console.log("The number is not positive");
+}
+
+let age = 18;
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote.");
+}
+
+let num = 10;
+if (num % 2 === 0) {
+  console.log("The number is even.");
+} else {
+  console.log("The number is odd.");
+}
