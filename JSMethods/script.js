feb294e2d587e429a4cfac8b66d54da46c166f41
mann08 <mann882005@gmail.com>
2026-05-01 18:48:42 +0530

    Add initial HTML and JavaScript files for JavaScript methods demonstration

---
+++ JSMethods/script.js
@@ -0,0 +1,15 @@
+let a=20,b=30;
+
+function sum(x,y) // with parameters
+{
+    let z = x+y;
+console.log(z);
+}
+
+sum(a,b);
+
+function welcome() // without parameters
+{
+    console.log("Welcome to JavaScript Methods");
+}
+welcome();
