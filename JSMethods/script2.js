2d9a2edbfd735fc42efd03ba6650dbe67671d205
mann08 <mann882005@gmail.com>
2026-05-01 19:41:18 +0530

    Refactor script2.js for improved readability; fix formatting and spacing in function expressions

--- JSMethods/script2.js
+++ JSMethods/script2.js
@@ -1,13 +1,22 @@
-let abc = function ()// function expression // ffunction hamesha const ke sath likhte h let ya var ke sath nahi likhte h
-{
-    console.log("Hello Mann");
+let abc = function () {
+  // function expression // ffunction hamesha const ke sath likhte h let ya var ke sath nahi likhte h
+  console.log("Hello Mann");
 };
 
 console.log(abc);
 abc();
 
-let add = function (x,y) {
-    return x+y;
+let add = function (x, y) {
+  return x + y;
 };
-console.log(add(5,10));
+console.log(add(5, 10));
 console.log(add);
+
+const arrowfun = () => {
+  console.log("This is an arrow function");
+};
+
+arrowfun();
+
+const arrowfun2 = (x, y) => x + y;
+console.log(arrowfun2(7, 8));
