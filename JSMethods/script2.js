d2e842f2dee977a923622a85736d62b5657f10b3
mann08 <mann882005@gmail.com>
2026-05-01 19:16:22 +0530

    Add script2.js with function expressions and example usage; update index.html to reference new script

---
+++ JSMethods/script2.js
@@ -0,0 +1,13 @@
+let abc = function ()// function expression // ffunction hamesha const ke sath likhte h let ya var ke sath nahi likhte h
+{
+    console.log("Hello Mann");
+};
+
+console.log(abc);
+abc();
+
+let add = function (x,y) {
+    return x+y;
+};
+console.log(add(5,10));
+console.log(add);
