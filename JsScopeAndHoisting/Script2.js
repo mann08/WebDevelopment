687c2bd0698fd7030b1fcfd38682ba5e3ef6c69d
mann08 <mann882005@gmail.com>
2026-05-03 23:27:37 +0530

    Refactor Index.html and Script2.js for improved formatting; adjust spacing and indentation for consistency

--- JsScopeAndHoisting/Script2.js
+++ JsScopeAndHoisting/Script2.js
@@ -2,26 +2,26 @@ let a;
 let b;
 let c;
 
-a=10;
+a = 10;
 console.log(a);
 console.log(b); // undefined
 console.log(c); // undefined
 
-b=15;
+b = 15;
 console.log(a);
 console.log(b);
 console.log(c); // undefined
 
-c=20;
+c = 20;
 console.log(a);
 console.log(b);
 console.log(c);
 
 abc();
 function abc() {
-    console.log("This is a function");
+  console.log("This is a function");
 }
 
 function submit() {
-    console.log("Submit button clicked");
+  console.log("Submit button clicked");
 }
