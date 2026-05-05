340186a908c776a4af05a8a652835d1a1e4aa626
mann08 <mann882005@gmail.com>
2026-05-05 19:22:46 +0530

    Add HTML and JavaScript files for array manipulation; implement basic array operations and structure

---
+++ JSArray/Script.js
@@ -0,0 +1,22 @@
+// let arr = [1, 2, 3, 4, 5];
+// for(var i = 0; i<arr.length; i++)
+//     {
+//     console.log(arr[i]);
+// }
+// console.log(arr);
+
+// arr[2] = arr[2]+10;
+// console.log(arr);
+
+// arr = [];
+// console.log(arr);
+
+let arr = [10,20,30,40,50,11,8,16,9]
+arr.push(1108);
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.unshift(1);
+console.log(arr);
+arrshift();
+console.log(arr);
