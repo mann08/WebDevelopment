b8a9cccde50dc81350837417cebc2004b326c327
mann08 <mann882005@gmail.com>
2026-05-07 19:35:22 +0530

    Add initial HTML, CSS, and JavaScript files for bulb control; implement basic event handling and styling

---
+++ JSEventHandling/Script.js
@@ -0,0 +1,16 @@
+function Bulb1ON(){
+    document.getElementById("bulb1").style.backgroundColor = "yellow";
+
+}
+function Bulb1OFF(){
+    document.getElementById("bulb1").style.backgroundColor = "white";
+}
+function warning(){
+    document.getElementById("bulb1").style.backgroundColor = "red";
+}
+function Success(){
+    document.getElementById("bulb1").style.backgroundColor = "green";
+}
+function Secondary(){
+    document.getElementById("bulb1").style.backgroundColor = "blue";
+}
