c506701de9c541088e55a8bacf429fd5af1bf190
mann08 <mann882005@gmail.com>
2026-05-15 20:39:13 +0530

    Refactor task management functions; enhance task display and deletion logic, and improve local storage handling

--- Projects/To_Do_List/script.js
+++ Projects/To_Do_List/script.js
@@ -1,22 +1,82 @@
-function addNewTask(){
-    const taskToAdd = document.getElementById("newTask").value;
+function addNewTask() {
+  const taskToAdd = document.getElementById("newTask").value;
 
+  const LI = document.createElement("li");
+  LI.classList.add("my-3");
+
+  const DIV = document.createElement("div");
+  DIV.classList.add("d-flex", "align-items-center");
+
+  const SPAN1 = document.createElement("span");
+  SPAN1.innerText = taskToAdd;
+
+  const BUTTON = document.createElement("button");
+  BUTTON.classList.add(
+    "btn",
+    "btn-danger",
+    "ms-4",
+    "d-flex",
+    "gap-3",
+    "align-items-center",
+    "justify-content-center",
+  );
+  BUTTON.onclick = () => {
+    LI.remove();
+    removeTaskFromLocalStorage(taskToAdd);
+  };
+
+  const I = document.createElement("i");
+  I.classList.add("bi", "bi-trash");
+
+  document.getElementById("taskList").appendChild(LI);
+
+  saveToLocalStorage(taskToAdd);
+
+  document.getElementById("newTask").value = "";
+}
+
+function saveToLocalStorage(taskToAdd) {
+  const TaskArray = JSON.parse(localStorage.getItem("toDoTask")) || [];
+
+  TaskArray.push(taskToAdd);
+
+  const newTaskArray = JSON.stringify(TaskArray);
+
+  localStorage.setItem("toDoTask", newTaskArray);
+}
+
+function getTaskFromLocalStorage() {
+  const TaskArray = JSON.parse(localStorage.getItem("toDoTask")) || [];
+
+  console.log(TaskArray);
+
+  TaskArray.forEach((element) => {
     const LI = document.createElement("li");
     LI.classList.add("my-3");
 
     const DIV = document.createElement("div");
     DIV.classList.add("d-flex", "align-items-center");
 
-    const SPAN = document.createElement("span");
-    SPAN.innerText = taskToAdd;
+    const SPAN1 = document.createElement("span");
+    SPAN1.innerText = element;
 
     const BUTTON = document.createElement("button");
-    BUTTON.classList.add("btn","btn-danger","ms-4","d-flex","gap-3","align-items-center","justify-content-center");
+    BUTTON.classList.add(
+      "btn",
+      "btn-danger",
+      "ms-4",
+      "d-flex",
+      "gap-3",
+      "align-items-center",
+      "justify-content-center",
+    );
+    BUTTON.onclick = () => {
+      LI.remove();
+      removeTaskFromLocalStorage(element);
+    };
 
-    BUTTON.onclick = () => LI.remove();
-    
     const I = document.createElement("i");
-    I.classList.add("bi" , "bi-trash")
+    I.classList.add("bi", "bi-trash");
 
     const SPAN2 = document.createElement("span");
     SPAN2.innerText = "Delete";
@@ -24,37 +84,31 @@ function addNewTask(){
     BUTTON.appendChild(I);
     BUTTON.appendChild(SPAN2);
 
-    DIV.appendChild(SPAN);
+    DIV.appendChild(SPAN1);
     DIV.appendChild(BUTTON);
 
     LI.appendChild(DIV);
-    document.getElementById("")
+
+    document.getElementById("taskDiv").classList.remove("d-none");
 
     document.getElementById("taskList").appendChild(LI);
-
-    saveToLocalStorage(taskToAdd);
-
-    document.getElementById("newTask").value;
-
+  });
 }
 
-function saveToLocalStorage(taskToAdd){
+getTaskFromLocalStorage();
 
+function removeTaskFromLocalStorage(taskItem) {
+  console.log(taskItem);
 
-    const TaskArray = JSON.parse(localStorage.getItem("toDoTask")) || [];
-   
-    TaskArray.push(taskToAdd);
+  const TaskArray = JSON.parse(localStorage.getItem("toDoTask"));
 
-    console.log(TaskArray);
+  console.log(TaskArray);
 
-    const newTaskArray = JSON.stringify(TaskArray);
+  const newTaskArray = TaskArray.filter((item) => item !== taskItem);
 
-    localStorage.setItem("toDoTask" , newTaskArray);
+  console.log(newTaskArray);
+
+  const newTaskString = JSON.stringify(newTaskArray);
+
+  localStorage.setItem("toDoTask", newTaskString);
 }
-
-function getTaskFormLocalStorage(){
-    const TaskArray = JSON.parse(localStorage.getItem("toDoTask")) || [];
-
-    console.log(TaskArray); 
-}
-getTaskFormLocalStorage();
