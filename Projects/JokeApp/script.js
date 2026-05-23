f3de0b9cf50de558faf3e6315a360473beb16a0b
mann08 <mann882005@gmail.com>
2026-05-23 20:59:59 +0530

    Fix HTML structure and update GetNewJoke function to fetch jokes from the official API

--- Projects/JokeApp/script.js
+++ Projects/JokeApp/script.js
@@ -1,5 +1,5 @@
 async function GetNewJoke() {
-  const API_URL = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark";
+  const API_URL = "https://official-joke-api.appspot.com/jokes/random";
 
   const response = await fetch(API_URL);
   // console.log(response);
@@ -7,14 +7,16 @@ async function GetNewJoke() {
   const data = await response.json();
   // console.log(data);
 
-//   if (data.type === "single") {
-//     document.getElementById("setup").innerText = data.joke;
-//     document.getElementById("delivery").innerText = "";
-//   } else {
-//     document.getElementById("setup").innerText = data.setup;
-//     document.getElementById("delivery").innerText = data.delivery;
-//   }
+  //   if (data.type === "single") {
+  //     document.getElementById("setup").innerText = data.joke;
+  //     document.getElementById("delivery").innerText = "";
+  //   } else {
+  //     document.getElementById("setup").innerText = data.setup;
+  //     document.getElementById("delivery").innerText = data.delivery;
+  //   }
 
-   document.getElementById("setup").innerText = data.joke || data.setup;
-    document.getElementById("delivery").innerText = data.delivery ||"";
+  document.getElementById("setup").innerText ="Setup : " + (data.joke || data.setup);
+  document.getElementById("delivery").innerText ="Punchline: " + (data.punchline || "");
+  document.getElementById("type").innerText = "Type: " + data.type;
+  document.getElementById("id").innerText = "ID: " + data.id;
 }
