c3e61c81751afe7e20cf1ed70792ec99618490cc
mann08 <mann882005@gmail.com>
2026-06-01 19:32:57 +0530

    Update App component to include all pages and fix Header component styling and typo

--- Frontend/myFirstReactApp/src/App.jsx
+++ Frontend/myFirstReactApp/src/App.jsx
@@ -1,11 +1,22 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import ContactUS from "./pages/ContactUS";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
       <Header />
-      
+      <Home />
+      <About />
+      <ContactUS />
+      <Product />
+      <Login />
+      <Register />
       <Footer />
     </>
   );
--- Frontend/myFirstReactApp/src/components/Header.jsx
+++ Frontend/myFirstReactApp/src/components/Header.jsx
@@ -1,13 +1,13 @@
 function Header() {
   return (
     <>
-      <div className="bg-bg-primary-subtle p-2 d-flex justify-content-between align-items-center">
+      <div className="bg-primary-subtle p-2 d-flex justify-content-between align-items-center">
         <div className=" text-primary fs-4 fw-bold">My Company </div>
         <div className="d-flex gap-4">
           <span>Home</span>
           <span>About</span>
           <span>Products</span>
-          <span>ConatactUS</span>
+          <span>Conatact Us</span>
         </div>
         <div className="d-flex gap-3">
           <button className="btn btn-outline-primary">Login</button>
