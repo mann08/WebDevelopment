eede652beb7bad8d9657c6977db28e4b922f8656
mann08 <mann882005@gmail.com>
2026-06-05 22:54:04 +0530

    style: improve code formatting for consistency in HTML, CSS, and JavaScript files

--- Projects/RandomQuoteGenerator/script.js
+++ Projects/RandomQuoteGenerator/script.js
@@ -1,22 +1,19 @@
 const quotes = [
-    "Success is the sum of small efforts repeated daily.",
-    "Dream big and dare to fail.",
-    "Learning never exhausts the mind.",
-    "Stay hungry, stay foolish.",
-    "Code today, lead tomorrow.",
-    "Every expert was once a beginner.",
-    "Consistency beats motivation.",
-    "Your future is created by what you do today."
+  "Success is the sum of small efforts repeated daily.",
+  "Dream big and dare to fail.",
+  "Learning never exhausts the mind.",
+  "Stay hungry, stay foolish.",
+  "Code today, lead tomorrow.",
+  "Every expert was once a beginner.",
+  "Consistency beats motivation.",
+  "Your future is created by what you do today.",
 ];
 
 const quoteText = document.getElementById("quote");
 const button = document.getElementById("btn");
 
 button.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
 
-    const randomIndex = Math.floor(
-        Math.random() * quotes.length
-    );
-
-    quoteText.textContent = quotes[randomIndex];
+  quoteText.textContent = quotes[randomIndex];
 });
