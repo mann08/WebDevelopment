eb0b523d08822bac15d2d993242c4520baa32ef5
mann08 <mann882005@gmail.com>
2026-06-10 20:26:53 +0530

    feat: implement main application structure with routing and components

--- Frontend/FakeStore/src/App.jsx
+++ Frontend/FakeStore/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Header from './components/Header'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Products from './Pages/Products'
+import Footer from './components/Footer'
+import BrowserRouter, { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-<>
-    <div>App</div>
-</>
+    <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+        <Footer />
+    </BrowserRouter>
   )
 }
 
