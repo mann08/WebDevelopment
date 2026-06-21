a694f5d02289f9f0e862cea3f90f2820e4b15697
mann08 <mann882005@gmail.com>
2026-06-21 17:26:02 +0530

    chore: initialize package.json for first backend project with dependencies and scripts

---
+++ Backend/firstBackend/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Default Get API Hit");
+  res.json({ message: "Welcome to my first backend project" });
+});
+
+app.post("/login", (req, res) => {
+  res.json({ message: "Login Succesfull" });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Server Started on port : ", port);
+});
