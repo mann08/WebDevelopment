7757e1d8f796221aa3b49509a2e27b710643016c
mann08 <mann882005@gmail.com>
2026-06-22 19:14:47 +0530

    feat: add authentication and public routers for handling user and contact requests

--- Backend/firstBackend/index.js
+++ Backend/firstBackend/index.js
@@ -2,29 +2,35 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import AuthRouter from "./src/routers/auth.route.js";
+import PublicRouter from "./src/routers/public.route.js";
 
 const app = express();
 
+app.use("/auth", AuthRouter);
+app.use("/public", PublicRouter);
+
+// Default API
 app.get("/", (req, res) => {
   console.log("Default Get API Hit");
   res.json({ message: "Welcome to my first backend project" });
 });
 
-app.post("/login", (req, res) => {
-  res.json({ message: "Login Succesfull" });
-});
-app.post("/logout", (req, res) => {
-  res.json({ message: "Logout Succesfull" });
-});
-app.post("/register", (req, res) => {
-  res.json({ message: "Register Succesfull" });
-});
-app.put("/update", (req, res) => {
-  res.json({ message: "Update Succesfull" });
-});
-app.delete("/delete", (req, res) => {
-  res.json({ message: "Delete Succesfull" });
-});
+// app.post("/login", (req, res) => {
+//   res.json({ message: "Login Succesfull" });
+// });
+// app.post("/logout", (req, res) => {
+//   res.json({ message: "Logout Succesfull" });
+// });
+// app.post("/register", (req, res) => {
+//   res.json({ message: "Register Succesfull" });
+// });
+// app.put("/update", (req, res) => {
+//   res.json({ message: "Update Succesfull" });
+// });
+// app.delete("/delete", (req, res) => {
+//   res.json({ message: "Delete Succesfull" });
+// });
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
---
+++ Backend/firstBackend/src/routers/public.route.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post("/contact-us");
+
+export default router;
