7c043e30fe006c3f8b36e76df416ee23457c4e17
mann08 <mann882005@gmail.com>
2026-06-24 23:33:16 +0530

    feat: add user model, database connection, and auth controller
    - Initialize MongoDB connection configuration
    - Implement User schema with required fields
    - Create authentication controller for login and registration

--- Backend/firstBackend/src/controllers/auth.controller.js
+++ Backend/firstBackend/src/controllers/auth.controller.js
@@ -1,9 +1,25 @@
+import User from "../models/user.model.js";
+
+export const RegisterUser = async (req, res) => {
+  try {
+    const { fullName, email, phone, gender, password, dob } = req.body;
+    if (!fullName || !email || !phone || !password || !dob || !gender) {
+      res.status(400).json({ message: "All Fields are Required" });
+      return;
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(409).json({ message: "Email Already Registered" });
+      return;
+    }
+    // Create New User and Complete Registration will do this tomorrow
+  } catch (error) {}
+};
+
 export const LoginUser = (req, res) => {
   res.json({ message: "Login Successfull From Controller" });
 };
 export const LogoutUser = (req, res) => {
   res.json({ message: "Logout Successfull From Controller" });
-};
-export const RegisterUser = (req, res) => {
-  res.json({ message: "Register Successfull From Controller" });
 };
